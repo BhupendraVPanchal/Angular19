e98bbbb4d4eb2d3835c37652de5c30d551c6765e
Bhupendra-Mindtech <bhupendra.p@mindtechsolutions.com>
2025-06-11 14:30:02 +0530

    ok

--- Helper_CL/obj/Debug/net8.0/Helper_CL.AssemblyInfo.cs
+++ Helper_CL/obj/Debug/net8.0/Helper_CL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helper_CL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a846f9b800b4b7114e5f6846aec9b7e558a6c021")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helper_CL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helper_CL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
